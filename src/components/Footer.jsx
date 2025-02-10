b6b00ecf1ecc6bf3e54b33eb6f7f6c6711862c42
theahmed619 <theahmed619@gmail.com>
2025-02-10 17:52:18 +0530

    add links in contact section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,15 @@ const Footer = () => {
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-14 ">
           <div className=" flex flex-col items-center justify-center">
             <div className="flex space-x-4">
-              <FaTwitter size={25} />
-              <FaGithub size={25} />
-              <FaLinkedin size={25} />
+              <a href="https://x.com/theahmedshaikh" target="_blank">
+                <FaTwitter size={25} />
+              </a>
+              <a href="https://github.com/theahmed619" target="_blank">
+                <FaGithub size={25} />
+              </a>
+              <a href="https://www.linkedin.com/in/ahmed619/" target="_blank">
+                <FaLinkedin size={25} />
+              </a>
             </div>
             <div className="mt-3 border-t border-gray-700 pt-8 flex-col items-center">
               <p>Thanks for visit ❤️</p>
